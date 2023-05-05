dd1ac329dc6ffb8341ee9938963e749bf6358121
David Calaiò <davidcjr11@gmail.com>
2023-05-06 00:27:55 +0200

    fixed cards

--- src/components/partials/mostPop.vue
+++ src/components/partials/mostPop.vue
@@ -23,9 +23,11 @@ export default{
     <div class="dc-card d-flex">
         <div class="posters">
             <img :src="`https://image.tmdb.org/t/p/w500${image}`" alt="" class="poster">
-            <img :src= "`/flags/language-${lang}.svg`" :alt="{lang}" class="flags">
+            <div class="card-text">
+                <img :src= "`/flags/language-${lang}.svg`" :alt="{lang}" class="flags">
+                <span>{{ vote }} /5</span>
+            </div>
             <h5>{{ title }}</h5>
-            <span>{{ vote }} /5</span>
         </div>
     </div>
   
@@ -39,29 +41,40 @@ export default{
         width: 100%;
         height: 100%;
         margin-bottom: 20px;
-        padding-left: 0;
-        list-style: none;
+        position: relative;
+        
+        .card-text{
+            width: 30%;
+            position: absolute;
+            overflow: hidden;
+            transition: all 1s;
+            span{
+                width: 100%;
+                height: 100%;
+                color: white;
+                position: absolute;
+                bottom: 50px;
+                left: 10px;
+            }
+            .flags{
+                position: absolute;
+                bottom: 50px;
+                left: 10px;
+                width: 30px;
+                height: 50px;
+            }
+        }
         h5{
             padding-top: 10px;
             color: white;
         }
-        span{
-            width: 100%;
-            height: 100%;
-            color: white;
-            position: absolute;
-            top: 100px;
-            left: 10px;
-            opacity: 1;
-            
-        }
         .poster{
-            position: relative;
             padding-top: 20px;
             height: 100%;
             width: 100%;
             object-fit: contain;
             max-height: 150px;
+            transition: all 2s;
             transition: transform 450ms;
             &:hover{
                 cursor: pointer;
@@ -69,12 +82,10 @@ export default{
                 transform: scale(1.10);
             }
         }
-        .flags{
-            position: absolute;
-            top: 60px;
-            left: 10px;
-            width: 30px;
-            height: 50px;
+        &:hover{
+            .card-text{
+                overflow: visible;
+            }
         }
     }
 
